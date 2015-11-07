b20747e963f616b3c948a51b0b900866906c941b
Eric Andrade <eric@diskcidades.com>
2015-11-06 23:59:05 -0200

    Mudancas de pastas

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# formValidator
+# FormValidator
 Plugin in jQuery to validate your forms using ajax or not. A validation differently!
+
+### Features
+* Validação de qualquer formulário;
+* Não necessita de CSS;
+* Possível criar mensagem de confirmação ao submiter formulário (opcional);
+* Possível submeter em **AJAX** ou padrão;
+* **Callbacks** para o método AJAX (success e error);
+* Permite criar suas próprias validações (personalizados);
+* Métodos específicos e personalizados para Sucesso e Erro de validação;
+* Método para realizar ações **antes** e **depois** para o método AJAX;
+* Disponível em **3 idiomas**: português(**pt-bt**), inglês(**en**), espanhol(**es**);
+
+### Particularidades
+* Funciona somente com biblioteca jQuery;
+* Funciona somente com atributo ID (ex.: ``` <form id="my-form"></form> ```);
+
+### Usage
+  * **Include in header your Html**:
+```html
+  <script src="js/jquery.formValidator.js"></script>
+```
+  * **Form (Html)**:
+```html
+  <form name="my-form" id="my-form" method="POST" action="send.php">
+    <form>
+    
+    </form>
+  </form>
+```
---
+++ src/jquery.formValidator.js
@@ -0,0 +1,197 @@
+//PLUGIN PARA VALIDAÇÃO DE FORMULARIOS CRIADO POR ERIC ANDRADE - ericferreira1992@gmail.com
+"use strict";
+$.fn.formValidator = function(options) {
+    var defaults = {
+        before: function(){},
+        after: {
+            successValidation: '',
+            errorValidation: ''
+        },
+        submitEnable: true,
+        confirmSubmit : '',
+        thisForm: this,
+        erro: false,
+        alertaErro: '',
+        myValidations: [],
+        sending: {
+            type: 'redirect',
+            dataType: 'html',
+            success: '',
+            error: ''
+        },
+        lang:'pt-bt'
+    };
+    var settings = $.extend( {}, defaults, options );
+
+    settings.thisForm = this;
+    $(document).on('submit', '#'+settings.thisForm.attr('id'), function(e){
+        if(settings.submitEnable){
+            if(settings.confirmSubmit!=''){
+                if(!window.confirm(settings.confirmSubmit))
+                    return false;
+            }
+
+            if (typeof settings.before == 'function') {
+                settings.before.call(this);
+            }
+
+            settings.erro = false;
+            settings.alertaErro = languages[settings.lang].precisam_ser_preenchidos;
+            $(this).find('input[data-required="true"], select[data-required="true"], textarea[data-required="true"]').map(function(){
+                if($(this).attr('type')=="radio"){
+                    //Buscar quantidade com o mesmo nome e ver se há algum checkado!!
+                    if($(this).prop('checked')==false){
+                        if($(this).data("title")!="" && $(this).data("title")!=undefined)
+                            var nome = $(this).data("title");
+                        else
+                            var nome = $(this).parent().children('label').text();
+
+                        var checked = false
+                        $(this).find('input[name="'+$(this).attr("name")+'"]').map(function(){
+                            if($(this).prop('checked')==true){
+                                checked =  true;
+                            }
+                        });
+                        if(!checked){
+                            settings.erro = true;
+                            settings.alertaErro = settings.alertaErro.replace("- "+nome+"\n",'');
+                            settings.alertaErro += "- "+nome+"\n";
+                        }
+                    }
+                }
+                else if($(this).attr('type')=="checkbox"){
+                    if($(this).prop('checked')==false){
+                        settings.erro = true;
+                        if($(this).data("title")!="" && $(this).data("title")!=undefined)
+                            var nome = $(this).data("title");
+                        else
+                            var nome = $(this).parent().children('label').text();
+                        settings.alertaErro = settings.alertaErro.replace("- "+nome+"\n",'');
+                        settings.alertaErro += "- "+nome+"\n";
+                    }
+                }
+                else{
+                    if($(this).val()==""){
+                        settings.erro = true;
+                        if($(this).data("title")!="" && $(this).data("title")!=undefined)
+                            var nome = $(this).data("title");
+                        else
+                            var nome = $(this).parent().children('label').text();
+                        settings.alertaErro += "- "+nome+"\n";
+                    }
+                }
+            });
+
+            if(!settings.erro){
+                settings.alertaErro = "";
+                settings.alertaErro = languages[settings.lang].precisam_ser_iguais;
+                $(this).find('input[data-equals], select[data-equals], textarea[data-equals]').map(function(){
+                    var inputNameIgual = $(this).data("equals");
+                    if($(this).val() != $('input[name="'+inputNameIgual+'"]').val()){
+                        settings.erro = true;
+                        if($(this).data("title")!="" && $(this).data("title")!=undefined)
+                            var nomeInput = $(this).data("title");
+                        else
+                            var nomeInput = $(this).parent().children('label').text();
+
+                        if($('input[name="'+inputNameIgual+'"]').data("title")!="" && $('input[name="'+inputNameIgual+'"]').data("title")!=undefined)
+                            var nomeInputIgual = $('input[name="'+inputNameIgual+'"]').data("title");
+                        else
+                            var nomeInputIgual = $('input[name="'+inputNameIgual+'"]').parent().children('label').text();
+                        settings.alertaErro += "- "+nomeInputIgual+" e "+nomeInput+"\n";
+                    }
+                });
+            }
+
+            if(!settings.erro){
+                settings.alertaErro = languages.getAttribute(settings.lang).valores_invalidos;
+                $(this).find('input[data-min-characters], textarea[data-min-characters]').map(function(){
+                    var minCharacters = parseInt($(this).data("min-characters"));
+                    if($(this).val().length < minCharacters){
+                        settings.erro = true;
+                        if($(this).data("title")!="" && $(this).data("title")!=undefined)
+                            var nomeInput = $(this).data("title");
+                        else
+                            var nomeInput = $(this).parent().children('label').text();
+
+                        settings.alertaErro += "- "+nomeInput+" precisa ter no mínimo "+minCharacters+" caractéres.\n";
+                        settings.alertaErro = (languages[settings.lang].precisa_no_minimo_caract).replace('nomeInput', nomeInput).replace('minCharacters', minCharacters);
+                    }
+                });
+            }
+
+            if(!settings.erro){
+                if(settings.myValidations.length>0){
+                    settings.alertaErro = languages[settings.lang].valores_invalidos;
+                    for(var i=0; i<settings.myValidations.length; i++){
+                        if(typeof settings.myValidations[i] == 'function'){
+                            var msg = settings.myValidations[i].call(this);
+                            if(msg != "" && msg!=undefined){
+                                settings.alertaErro += "- "+msg+"\n";
+                                settings.erro = true;
+                            }
+                        }
+                    }
+                }
+            }
+            done();
+        }
+        e.stopImmediatePropagation();
+        return false;
+    });
+
+    var ajaxingForm = false;
+    function done(){
+        if(!settings.erro){//SUCCESS
+            if (typeof settings.after.successValidation == 'function') {
+                settings.after.successValidation.call(this);
+            }
+            if(settings.sending.type == "ajax"){
+                var inputs = $('#'+settings.thisForm.attr('id')).serialize();
+                var url = $(settings.thisForm).attr('action');
+                var method = ($(settings.thisForm).attr('method')=="" || $(settings.thisForm).attr('method')==undefined) ? "POST" : $(settings.thisForm).attr('method');
+                if(!ajaxingForm){
+                    ajaxingForm = true;
+                    $.ajax({
+                        type: method,
+                        url: url,
+                        data: inputs,
+                        dataType: settings.sending.dataType,
+                        success: function(retorno) {
+                            ajaxingForm = false;
+                            if (typeof settings.sending.success == 'function') {
+                                settings.sending.success(retorno);
+                            }
+                        },
+                        error: function(){
+                            ajaxingForm = false;
+                            if (typeof settings.sending.error == 'function') {
+                                settings.sending.error(retorno);
+                            }
+                        }
+                    });
+                }
+            }
+            else{
+                settings.submitEnable = false;
+                $(settings.thisForm).submit();
+            }
+        }
+        else{//ERROR
+            if (typeof settings.after.errorValidation == 'function') {
+                settings.after.errorValidation.call(this);
+            }
+            alert(settings.alertaErro);
+        }
+    }
+
+    var languages = {
+        'pt-bt':{
+            'precisam_ser_preenchidos':"Os seguintes campos precisam ser preenchidos:\n",
+            'valores_invalidos':"Ops, ocorreram alguns erros:\n",
+            'precisam_ser_iguais':"Os campos abaixo precisam ser iguais:\n",
+            'precisa_no_minimo_caract':"- nomeInput precisa ter no mínimo minCharacters caractéres.\n",
+            'ocorreu_erro':"Ops, ocorreram alguns erros:\n"
+        }
+    };
+};
